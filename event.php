1d064068aa220b412ea628d40a92521b74ad9158
ChesterChin92 <chinschian@hotmail.com>
2016-02-11 02:32:47 +0800

    Added event.php and even.css

---
+++ event.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<head>
+
+    <link href="styles/event.css" rel="stylesheet">
+
+    <title>Simple Home</title>
+</head>
+<body>
+
+<div class='nav'>
+    <ul>
+        <li><a href='#link'><span>Home</span></a></li>
+        <li><a href='#link'><span>About Us</span></a></li>
+        <li class='active'><a href='#link'><span>Events</span></a></li>
+        <li><a href='#link'><span>Info</span></a></li>
+        <li class='last'><a href='#link'><span>Contact Us</span></a></li>
+    </ul>
+</div>
+</body>
+
+<style>
+body {
+
+    background-image: url("leehom.jpg");
+    background-repeat: no-repeat;
+    background-position: right top;
+    margin-right: 300px;
+}
+</body>
+</style>
+
+</br>
+
+<body>
+</br>
+</br>
+<div class='info'>
+	<h1>Lee Hom's concert</h1>
+		<p>Event Venue:<strong> Taiwan</strong></p></br>
+		<p>Event Date : <strong> 10th May 2016</strong></p>
+</div>
+
+</br>
+</br>
+
+<p>Please select registrants or attendees:</p>
+	<div class='dropdown'>
+		  <button class="dropbtn">Registrants/Attendees</button>
+		  <div class="dropdown-content">
+		    <a href="#">Registrants</a>
+		    <a href="#">Attendees</a>
+		  </div>
+	</div>
+
+</br>
+</br>
+
+<a href="#" class="button1">Forum</a>
+
+
+</body>
+
+<html>
