82a5f8b6f7d1fb1742dc628f0ff292d3400565f9
swee yinn <swee yinn>
2016-02-11 00:33:57 +0800

    Added task 3 source code

---
+++ task3.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+
+<link rel="stylesheet"  href="task3.css"/>
+<title> Registration page</title>
+
+<style>
+body {
+    background-image: url("leehom.jpg");
+    background-repeat: no-repeat;
+    background-position: right top;
+    margin-right: 300px;
+}
+</head>
+</style>
+
+<body>
+<ul>
+  <li><a href="#home">Home</a></li>
+  <li><a class="active" href="#event">Event</a></li>
+  <li><a href="#about">About</a></li>
+</ul>
+
+</br>
+</br>
+	<h1>Lee Hom's concert</h1>
+	<p>Event Venue: Taiwan</p>
+	<p>Event Date: 10th May 2016</p>
+
+</br>
+</br>
+
+<p>Please select registrants or attendees:</p>
+
+<div class="dropdown">
+  <button class="dropbtn">Registrants/Attendees</button>
+  <div class="dropdown-content">
+    <a href="#">Registrants</a>
+    <a href="#">Attendees</a>
+  </div>
+</div>
+
+</br>
+<a href="#" class="button1">Forum</a>
+<a href="#" class="button2">X</a>
+<a href="#" class="button3">+</a>
+
+<!---for image button
+<button type="submit" style="border: 0; background: transparent">
+    <img src="/images/times_button.PNG" width="90" height="50" alt="submit" />
+</button>
+-->
+</body>
+
+
+
+</body>
+</html>
+
+
+
+
